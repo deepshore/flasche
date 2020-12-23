66513cfb4289c73292f85137c90bebc85729bf8f
grothesk <malte.groth@deepshore.de>
2020-12-23 14:45:31 +0100

    Add build and run as CLI commands.

--- src/flasche/cli.py
+++ src/flasche/cli.py
@@ -1,5 +1,8 @@
 import click
 
+from .build import ProjectBuilder
+from .run import ProjectRunner
+
 
 @click.group()
 def main():
@@ -7,12 +10,19 @@ def main():
 
 
 @main.command()
-@click.argument('name')
-def add(name):
-    click.echo("add {n}".format(n=name))
+@click.argument('project')
+@click.option('-p', '--port', default=8000)
+def build(project, port):
+    click.echo("build project {p}".format(p=project))
+    builder = ProjectBuilder(project, port)
+    builder.build()
+    click.echo("done")
+    click.echo("run the app by \"flasche run {p}\"".format(p=project))
 
 
 @main.command()
-@click.argument('name')
-def init(name):
-    click.echo("init {n}".format(n=name))
+@click.argument('project')
+def run(project):
+    click.echo("run project {p}".format(p=project))
+    runner = ProjectRunner(project)
+    runner.run()
