f1cfc16a173041afc9b0489d36aeb1ed02079a3d
grothesk <malte.groth@deepshore.de>
2021-01-13 11:51:21 +0100

    Add some lines to the tests.

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,4 +1,6 @@
 import os
+from unittest.mock import patch
+
 from flasche.build import ProjectBuilder
 
 
@@ -11,3 +13,25 @@ def test_create_src_dst():
     src, dst = TEST_BUILDER.create_src_dst(test_path)
     assert dst == os.path.join(PROJECT, test_path)
     assert src == os.path.join(TEST_BUILDER.template_path, test_path)
+
+
+@patch(
+    'flasche.build.ProjectBuilder.create_src_dst',
+    return_value=('src', 'dst')
+)
+@patch('flasche.build.copy')
+def test_copy_main(copy, create_src_dst):
+    TEST_BUILDER.copy_main_py()
+    create_src_dst.assert_called_with('main.py')
+    copy.assert_called_with('src', 'dst')
+
+
+@patch(
+    'flasche.build.ProjectBuilder.create_src_dst',
+    return_value=('src', 'dst')
+)
+@patch('flasche.build.copytree')
+def test_copy_endpoints(copytree, create_src_dst):
+    TEST_BUILDER.copy_endpoints()
+    create_src_dst.assert_called_with('endpoints')
+    copytree.assert_called_with('src', 'dst')
--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,3 @@
-import os
 from unittest.mock import patch
 
 from flasche.run import ProjectRunner
