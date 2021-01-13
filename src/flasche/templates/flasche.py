82ed349deffc88686294cf5cdfc90fa2feff1c94
Frederic Born <frederic.born@nextevolution.de>
2021-01-13 14:41:35 +0100

    Change PORT in template due to flake8 F821

--- src/flasche/templates/flasche.py
+++ src/flasche/templates/flasche.py
@@ -4,7 +4,7 @@ from flasche import FlaskSetup
 APP_NAME = '{{ project }}'
 SWAGGER_TITLE = '{{ project }} API'
 SWAGGER_DESCRIPTION = 'Show the beauty of Swagger.'
-PORT = {{ port }}
+PORT = '{{ port }}'
 
 flask_setup = FlaskSetup(APP_NAME, PORT)
 flask_setup.add_swagger(SWAGGER_TITLE, SWAGGER_DESCRIPTION, url_prefix='/api/')
